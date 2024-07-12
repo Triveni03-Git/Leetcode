f1c1b335b1b04c1f52bf4f9361a7a4c0334ba347
Triveni03-Git <gaddamtriveni03@gmail.com>
2024-07-12 12:01:16 +0530

    Time: 15 ms (95.68%), Space: 57.4 MB (18.06%) - LeetHub

---
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minDifference(int[] nums) {
+        if (nums.length <= 4) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        return Math.min(
+            Math.min(nums[nums.length - 4] - nums[0], nums[nums.length - 1] - nums[3]),
+            Math.min(nums[nums.length - 3] - nums[1], nums[nums.length - 2] - nums[2])
+        );
+    }
+}
