cb26c0f296a46a8c19d2c40c660a6582f6a0098d
Triveni03-Git <gaddamtriveni03@gmail.com>
2024-11-16 00:58:53 +0530

    Time: 0 ms (100%), Space: 44.8 MB (44.26%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=1;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i-1]!=nums[j]){
+                nums[i++]=nums[j];
+            }
+        }
+        return i;
+    }
+}
